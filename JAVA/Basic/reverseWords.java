da4576a79da69540cce06f69c2ff00b9c0dea4c5
Jiwoong Yang <69667560+Woongstar@users.noreply.github.com>
2021-08-27 15:54:56 +0900

    Add files via upload

--- JAVA/Basic/reverseWords.java
+++ JAVA/Basic/reverseWords.java
@@ -31,14 +31,30 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class reverseWords {
-    public ArrayList<String> solution(int n, String[] words){
-        ArrayList<String> answer = new ArrayList<>();
-        for(String x: words){
-         String tmp = new StringBuilder(x).reverse().toString();
-         answer.add(tmp);
+//    public ArrayList<String> solution(int n, String[] words){
+//        ArrayList<String> answer = new ArrayList<>();
+//        for(String x: words){
+//         String tmp = new StringBuilder(x).reverse().toString();
+//         answer.add(tmp);
+//        }
+//        return answer;
+public ArrayList<String> solution(int n, String[] words) {
+    ArrayList<String> answer = new ArrayList<>();
+    for (String x : words) {
+        char[] s = x.toCharArray();
+        int lt = 0, rt = x.length() - 1;
+        while (lt < rt) {
+            char tmp = s[lt];
+            s[lt] = s[rt];
+            s[rt] = tmp;
+            lt++;
+            rt--;
         }
-        return answer;
+        String tmp = String.valueOf(s);
+        answer.add(tmp);
     }
+    return answer;
+}
     public static void main(String[] args) {
         reverseWords T = new reverseWords();
         Scanner scan = new Scanner(System.in);
