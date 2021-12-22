eaebdcc59cbf0ed5ba69ed4db902d0bbae5c756f
Jiwoong Yang <69667560+Woongstar@users.noreply.github.com>
2021-12-22 23:20:12 +0900

    BOJ1629, 재귀, 곱셈문제

---
+++ BOJ/BOJ1629.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class BOJ1629 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        Long a = Long.parseLong(st.nextToken());
+        Long b = Long.parseLong(st.nextToken());
+        Long c = Long.parseLong(st.nextToken());
+
+        System.out.println(cal(a,b,c)%c);
+    }
+    static Long cal(Long a, Long b, Long c){
+        if(b==0){
+            return 1L;
+        }else if(b==1){
+            return a;
+        }else if(b%2==0){
+            long n = cal(a,b/2,c)%c;
+            return n*n %c;
+        }else{
+            long n = cal(a, b / 2, c) % c;
+            return (((n * n) % c) * a) % c;
+        }
+    }
+}
