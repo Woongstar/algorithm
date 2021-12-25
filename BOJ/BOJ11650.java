febf5f5ec4d2dac482c61f7bf28d2cea549e9d2e
Jiwoong Yang <69667560+Woongstar@users.noreply.github.com>
2021-12-26 00:13:50 +0900

    BOJ11650, comparable,  좌표정렬

---
+++ BOJ/BOJ11650.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+class Point implements Comparable<Point> {
+    int x, y;
+
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public int compareTo(Point o) {
+        if (this.x == o.x) {
+            return this.y - o.y;
+        }
+        return this.x - o.x;
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+
+        ArrayList<Point> ar = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            ar.add(new Point(a, b));
+        }
+        Collections.sort(ar);
+        for (Point s : ar) {
+            System.out.println(s.x + " " + s.y);
+        }
+    }
+}
