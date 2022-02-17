dee8afcf218dda364d578fcfdfc73aa1a2a16ce5
Jiwoong Yang <69667560+Woongstar@users.noreply.github.com>
2022-02-17 21:13:51 +0900

    [Gold II] Title: 문제집, Time: 520 ms, Memory: 50340 KB -BaekjoonHub

--- 백준/Gold/1766.문제집/문제집.java
+++ 백준/Gold/1766.문제집/문제집.java
@@ -1,24 +1,25 @@
 import java.io.*;
 import java.util.*;
-import java.util.stream.Collectors;
 
 
-public class Main {
 
-
-    public static void main(String[] args) throws IOException {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+public class Main{
+    static ArrayList<List<Integer>> arr = new ArrayList<>();
+    static int[] level;
+    static BufferedWriter bw;
+    public static void main(String []args) throws Exception{
+        BufferedReader br =  new BufferedReader(new InputStreamReader(System.in));
+       bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
+       
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-
-        PriorityQueue<Integer> pQ = new PriorityQueue<>();
-        List<List<Integer>> arr = new ArrayList<List<Integer>>();
-        int[] level = new int[n+1];
-        for(int i=0;i<n+1;i++) arr.add(new ArrayList<>());
+        
+        level = new int[n+1];
+        
+        for(int i=0;i<=n;i++){
+            arr.add(new ArrayList<Integer>());
+        }
         while(m-->0){
             st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
@@ -26,12 +27,14 @@ public class Main {
             arr.get(x).add(y);
             level[y]++;
         }
-        topolSort(level, arr,n);
+        sort(n);
+     
         bw.flush();
         bw.close();
-    }
-    static void topolSort(int[] level,List<List<Integer>> arr,int n){
+     }
+    static void sort(int n) throws IOException{
         PriorityQueue<Integer> pQ = new PriorityQueue<>();
+        
         for(int i=1;i<=n;i++){
             if(level[i]==0){
                 pQ.add(i);
@@ -45,10 +48,7 @@ public class Main {
                     pQ.add(y);
                 }
             }
-            System.out.print(x+" ");
+            bw.write(x+" ");
         }
     }
 }
-
-
-
